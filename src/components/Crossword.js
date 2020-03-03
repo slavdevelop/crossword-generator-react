52f46f88b1ab549b270439409d138110bfe46926
Slavi Shtipkov <development@shtipkov.com>
2020-03-03 21:23:40 +0200

    Crossword words better look

--- src/components/Crossword.js
+++ src/components/Crossword.js
@@ -2,6 +2,15 @@ import React, {useContext} from 'react';
 
 import {GlobalContext} from "../context/GlobalState";
 
+const Word = ({wordText}) => {
+
+    return (
+        <span className="crossword-word">{wordText.split("").map(char => (
+            <span className="crossword-character">{char}</span>
+        ))}</span>
+    );
+}
+
 const Crossword = () => {
     const {words, isCrosswordGenerated} = useContext(GlobalContext);
 
@@ -9,7 +18,7 @@ const Crossword = () => {
 
     return (
         <div className="crossword-container">
-        
+            {words.map(word => <Word wordText={word} />)}
         </div>
     )
 }
