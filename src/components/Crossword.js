12fb724bbe3652d292d7d9dd07ad5e1923c9878a
Slavi Shtipkov <development@shtipkov.com>
2020-03-04 15:55:31 +0200

    Start constructing word object and checking for character matches

--- src/components/Crossword.js
+++ src/components/Crossword.js
@@ -15,6 +15,52 @@ const Box = ({char, row, col}) => {
 const Crossword = () => {
     const {words, isCrosswordGenerated} = useContext(GlobalContext);
 
+    const extendedWordsArray = [];
+
+    // Ordering the words by lenght DESC
+    const orderedWords = words.sort((a, b) => b.length - a.length);
+
+    for (let i = 0; i < orderedWords.length; i++) {
+        const wordObject = {
+            id: orderedWords[i],
+            text: orderedWords[i],
+            length: orderedWords[i].length
+        };
+        
+        // Check if it is the first one
+        if (i === 0) {
+            console.log(orderedWords[i], 'the first one');
+
+            // Calculating center coordinates of the grid
+            const middleRow = Math.round(rows / 2);
+            const middleCol = Math.round(cols / 2);
+
+            const startColCoordinate = middleCol - Math.round(wordObject.length / 2);
+
+            wordObject["isHorizontal"] = true;
+            wordObject["startRow"] = middleRow;
+            wordObject["startCol"] = startColCoordinate;
+
+            extendedWordsArray.push(wordObject);
+        } else {
+            const charArray = wordObject.text.split("");
+
+            extendedWordsArray.forEach(extendedWord => {
+
+                extendedWord.text.split("").forEach(extendedChar => {
+                    charArray.forEach(char => {
+                        if(extendedChar === char) {
+                            console.log(`Match! NW - ${wordObject.text} | EW - ${extendedWord.text} | char matched => ${char}`)
+                        }
+                    })
+                })
+
+            })
+        }
+        
+    }
+
+
     const createGrid = () => {
         const grid = [];
 
