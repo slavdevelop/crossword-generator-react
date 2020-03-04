8ec47c62a3a7b29aab410be0c84fc304633d027f
Slavi Shtipkov <development@shtipkov.com>
2020-03-04 14:14:45 +0200

    Add some testing default words

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,7 +3,7 @@ import React, {createContext, useReducer} from 'react';
 import AppReducer from "./AppReducer";
 
 const initialState = {
-    words: [], //['amazing', 'awesome', 'magic', 'outstanding', 'umbrella', 'test', 'words', 'any', 'kind', 'of', 'characters'],
+    words: ['amazing', 'awesome', 'magician', 'outstanding', 'umbrella', 'testing', 'words', 'anything', 'kind', 'offline', 'characters'],
     isCrosswordGenerated: false
 };
 
