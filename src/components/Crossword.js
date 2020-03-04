0ca839aa327a9da2c8564546e9a8d5c0cd8892cb
Slavi Shtipkov <development@shtipkov.com>
2020-03-04 13:53:33 +0200

    rendering empty crossword grid 20x20

--- src/components/Crossword.js
+++ src/components/Crossword.js
@@ -2,25 +2,50 @@ import React, {useContext} from 'react';
 
 import {GlobalContext} from "../context/GlobalState";
 
-const Word = ({wordText}) => {
+const rows = 20;
+const cols = 20;
+
+const Box = ({char, row, col}) => {
 
     return (
-        <span className="crossword-word">{wordText.split("").map(char => (
-            <span className="crossword-character">{char}</span>
-        ))}</span>
-    );
+        <span className={`crossword-character row-${row} col-${col}`}>{char}</span>
+    )
 }
 
 const Crossword = () => {
     const {words, isCrosswordGenerated} = useContext(GlobalContext);
 
+    const createGrid = () => {
+        const grid = [];
+
+        for (let row = 0; row < rows; row++) {
+            grid.push(new Array(cols));
+            
+            for (let col = 0; col < cols; col++) {
+                grid[row][col] = " ";
+            }
+            
+        }
+
+        return grid;
+    }
+
+    const grid = createGrid();
+    
     if (!isCrosswordGenerated) return '';
 
     return (
         <div className="crossword-container">
-            {words.map(word => <Word wordText={word} />)}
+            {grid.map((rowElement, rowNumber) => (
+                    <div className={`row row-${rowNumber}`}>
+                        {rowElement.map((colElement, colNumber) => {
+
+                            return <Box char={colElement} row={rowNumber} col={colNumber} />
+                        })}
+                    </div>
+            ))}
         </div>
     )
 }
 
-export default Crossword
+export default Crossword;
